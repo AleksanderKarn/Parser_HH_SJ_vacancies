2e50edc75d47f9e1a79fbbd27d1ff21858d8af02
Александр Карнаухов <118434162+AleksanderKarn@users.noreply.github.com>
2022-12-23 18:48:35 +0300

    файл main
    черновик мейна

---
+++ main.py
@@ -0,0 +1,14 @@
+### основное тело программы
+
+if __name__ == '__main__':
+    df = Connector('df.json')
+
+    data_for_file = {'id': 1, 'title': 'tet'}
+
+    df.insert(data_for_file)
+    data_from_file = df.select(dict())
+    assert data_from_file == [data_for_file]
+
+    df.delete({'id':1})
+    data_from_file = df.select(dict())
+    assert data_from_file == []
